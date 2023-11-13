29a571dd0a33dc17d27f66a4f3dd33eca2e32540
Wei Liang <133965494+weiliang8@users.noreply.github.com>
2023-11-13 12:53:02 +0000

    Changed port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ dotenv.config({ path: `${__dirname}/config/config.env` });
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1000;
 
 app.listen(
   PORT,
