24be2f3603f9dd55eb037d36fd89823f74864452
Wei Liang <133965494+weiliang8@users.noreply.github.com>
2023-11-19 14:38:46 +0800

    Backup

--- models/Bootcamps.js
+++ models/Bootcamps.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const geocoder = require("../utils/geocoder");
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -102,6 +103,16 @@ const BootcampSchema = new mongoose.Schema({
 // CREATE BOOTCAMP SLUG FROM THE NAME
 BootcampSchema.pre("save", function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// GEOCODE & CREATE LOCATION FIELD
+BootcampSchema.pre("save", async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: "Point",
+    coordinates: [],
+  };
   next();
 });
 
