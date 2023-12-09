d01c6d8a79832716254fbb4fbd818b1583f4f9e9
Chee Wei Liang <cheeweiliang8@gmail.com>
2023-12-09 14:48:51 +0800

    Forgot password -generate token

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { register, login, getMe } = require("../controller/auth");
+const { register, login, getMe,forgotPassword } = require("../controller/auth");
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ const { protect } = require("../middleware/auth");
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", protect, getMe);
+router.post("/forgotpassword",forgotPassword);
 
 module.exports = router;
