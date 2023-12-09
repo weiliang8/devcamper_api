bee3e4fc5f307d07b103082e30d177b3c96867c0
Chee Wei Liang <cheeweiliang8@gmail.com>
2023-12-09 19:20:18 +0800

    Update user details

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { register, login, getMe,forgotPassword,resetPassword} = require("../controller/auth");
+const { register, login, getMe,forgotPassword,resetPassword,updateDetails,updatePassword} = require("../controller/auth");
 
 const router = express.Router();
 
@@ -8,6 +8,8 @@ const { protect } = require("../middleware/auth");
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", protect, getMe);
+router.put("/updatedetails", protect, updateDetails);
+router.put("/updatepassword", protect, updatePassword);
 router.post("/forgotpassword",forgotPassword);
 router.put('/resetPassword/:resettoken',resetPassword)
 
