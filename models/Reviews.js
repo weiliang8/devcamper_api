1f2ad08fbf2d2e8d98f0771cb4fd07d0c4114280
Chee Wei Liang <cheeweiliang8@gmail.com>
2023-12-10 13:15:09 +0800

    Aggregate -calculate average rating

--- models/Reviews.js
+++ models/Reviews.js
@@ -36,4 +36,38 @@ const ReviewSchema = new mongoose.Schema({
 // PREVENT USER FROM SUBMITTING MORE THAN 1 REVIEW PER BOOTCAMP
 ReviewSchema.index({bootcamp:1,user:1},{unique:true})
 
+// STATIC METHOD TO GET AVG RATING AND SAVE
+ReviewSchema.statics.getAverageRating = async function (bootcampId) {
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageRating: { $avg: '$rating' }
+      }
+    }
+  ])
+  //console.log(obj)
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageRating: Math.ceil(obj[0].averageRating)
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+// CALL getAverageRating AFTER SAVE
+ReviewSchema.post('save', function () {
+  this.constructor.getAverageRating(this.bootcamp)
+})
+
+// CALL getAverageRating BEFORE REMOVE
+ReviewSchema.pre('save', function () {
+  this.constructor.getAverageRating(this.bootcamp)
+})
+
+
 module.exports = mongoose.model('Review', ReviewSchema);
